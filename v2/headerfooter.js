afc17cdfb1413b17b6c943e26bee89e7d5d11693
Hanley Weng <hanleyweng@gmail.com>
2017-09-07 20:01:33 -0400

    Added shared header and footer

---
+++ v2/headerfooter.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	var header = ''+
+	'<div id="site-header">'+
+		'<p>'+
+			'<a href="index.html">Home</a>'+
+			'<a href="ios.html">iPhone</a>'+
+			'<a href="mac.html">Mac</a>'+
+			'Android'+
+			'Windows'+
+			'<a href="about.html">About</a>'+
+		'</p>'
+	'</div>';
+	$('body').prepend(header);
+
+	var footer = ''+
+	'<div id="site-footer">'+
+		'<p>'+
+			'<a href="index.html">Home</a> | '+
+			'<a href="ios.html">iPhone & iPad</a> | '+
+			'<a href="mac.html">Mac</a> | '+
+			'Android | '+
+			'Windows<br>'+
+			'<a href="about.html">About</a> | '+
+			'<a href="mailto:cantonese.tools@gmail.com">Contact</a>'+
+		'</p>'
+		
+		'<p><em>© Cantonese Tools, 2017 – Updated October 2017</em></p>'+
+	'</div>'
+
+	$('body').append(footer);
+});
